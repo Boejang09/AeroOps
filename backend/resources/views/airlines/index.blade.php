aca9ef66a5dda469ce2b2af17232040b8eeb7d94
Boejang09 <hadiyantomuzamil@gmail.com>
2026-07-22 17:18:09 +0700

    feat(master): initialize airline module

---
+++ backend/resources/views/airlines/index.blade.php
@@ -0,0 +1,17 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            Airlines
+        </h2>
+    </x-slot>
+
+    <div class="py-6">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white shadow-sm rounded-lg p-6">
+                <h3>Airline Module</h3>
+
+                <p>Airline module is under development.</p>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
